9cd49a2c794a04c4029edec872a53d950630f7a1
Min Yang Jung <minyang.jung@intusurg.com>
2022-06-08 17:15:38 -0700

    Downgrade Ubuntu release from 20.04 back to 16.04 to support O2 build env

--- worker/build_mars_bb_worker.sh
+++ worker/build_mars_bb_worker.sh
@@ -4,10 +4,14 @@ USER_ID="min"
 
 set -e
 
+echo "Removing existing mars buildbot worker images"
+rm -f mars-buildbot-worker*.tar.gz
+
 docker build . -t mars-buildbot-worker:latest
 
 image="mars-buildbot-worker_$(git log -1 --pretty=%h).tar.gz"
 docker save mars-buildbot-worker:latest | gzip > $image
 
+# Publish built image
 scp $image $USER_ID@$HOST:~/www/buildbot
 ssh $USER_ID@$HOST "cd ~/www/buildbot && rm mars-buildbot-worker.tar.gz && ln -s $image mars-buildbot-worker.tar.gz"
